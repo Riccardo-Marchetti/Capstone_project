f05b3fd8728824eb342d5cc338088ee660a0f420
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-04-29 14:11:29 +0200

    creazione progetto

---
+++ BACK-END/src/main/java/riccardo/BACKEND/BackEndApplication.java
@@ -0,0 +1,13 @@
+package riccardo.BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
---
+++ BACK-END/src/test/java/riccardo/BACKEND/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardo.BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
