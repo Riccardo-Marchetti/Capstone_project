87efb776ca35413bda53d2714eb201d569588fd5
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-05-07 14:11:25 +0200

    Creazione register page

--- FRONT-END/src/App.jsx
+++ FRONT-END/src/App.jsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import LoginPage from "./components/LoginPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterPage from "./components/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
